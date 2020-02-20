efd269a59eedb2fcee41008760a9cabbf46f647f
Evan Higgins <evdhiggins@gmail.com>
2020-02-19 21:43:59 -0600

    Add localforage

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,9 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module'
+    '@nuxtjs/stylelint-module',
+    // Doc: https://github.com/nuxt-community/localforage-module
+    '@nuxtjs/localforage'
   ],
   /*
    ** Nuxt.js modules
